e6c0faf235c28257d66162a184a43010d1e8ab41
boom <linjieyang77@gmail.com>
2020-11-01 07:07:07 +0000

    feat: add bash script

---
+++ getIp.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+while true
+do
+	#The file that contains the current pubic IP
+	EXT_IP_FILE="./curIp.txt"
+	#Get the current public IP from whatsmyip.com
+	CURRENT_IP=$(curl http://ifconfig.me/ip)
+	#Check file for previous IP address
+	if [ -f $EXT_IP_FILE ]; then
+		KNOWN_IP=$(cat $EXT_IP_FILE)
+	else
+		KNOWN_IP=
+	fi
+	#See if the IP has changed
+	if [ "$CURRENT_IP" != "$KNOWN_IP" ]; then
+		echo $CURRENT_IP > $EXT_IP_FILE
+		#If so send an alert
+		echo "The IP Address at home has changed
+		The IP address at home has been changed to $CURRENT_IP"
+		git commit -am "[AUTO_COMMIT] IP change"
+		git pull
+		git push
+	else
+		#If not just report that it stayed the same
+		echo "The IP Address at home is the same
+		The IP address at home stayed the same $CURRENT_IP"
+	fi
+	echo "sleep 60s"
+	sleep 60
+done
