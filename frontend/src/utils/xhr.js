81d7f9a914fc32f59138665e0377a1e7a7fc1c03
boom <linjieyang77@gmail.com>
2020-11-02 07:52:10 +0000

    [AUTO_COMMIT] IP change

--- frontend/src/utils/xhr.js
+++ frontend/src/utils/xhr.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import {message,Modal} from "ant-design-vue";
 
 // export const baseUrl = 'http://127.0.0.1:3000';
-export const baseUrl = 'http://119.129.96.242:3000';
+export const baseUrl = 'http://219.136.175.249:3000';
 
 axios.defaults.baseURL = baseUrl;
 
